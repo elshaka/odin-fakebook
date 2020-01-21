30882376bc2f1a05630c941a09a6b4739f6fd728
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-21 13:34:16 -0400

    Added migration for missing attributes for user and post models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120192833) do
+ActiveRecord::Schema.define(version: 20200121173146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,11 +44,14 @@ ActiveRecord::Schema.define(version: 20200120192833) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+    t.string "profile_image_url"
   end
 
 end
