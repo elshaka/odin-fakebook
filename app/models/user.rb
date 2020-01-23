14593a73124dcabf5e52583d20b4767c349e4941
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2020-01-23 17:31:11 -0300

    Refactor 'User' model with a helper method 'friends'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,25 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :friendships
-  has_many :friends, through: :friendships, class_name: 'User'
   has_many :reverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
-  has_many :reverse_friends, through: :reverse_friendships, source: :user
 
   devise :database_authenticatable, :registerable, :validatable, :rememberable
 
   validates :name, presence: true, length: { maximum: 50 }
 
   before_save :set_profile_image_url
+
+  def friends
+    direct_friends = friendships.map do |friendship|
+      friendship.friend if friendship.confirmed
+    end
+
+    reverse_friends = reverse_friendships.map do |friendship|
+      friendship.user if friendship.confirmed
+    end
+
+    direct_friends + reverse_friends
+  end
 
   private
 
