1603160001243c52e5e2fc44fa9371e810a3fa9a
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2020-01-23 19:12:19 -0300

    Add 'send_friend_request' and 'accept_friend_request' actions to UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def send_friend_request
+    current_user.friendships.create friend_id: params[:id]
+  end
+
+  def accept_friend_request
+    current_user.reverse_friendships.find_by(id: params[:id])
+  end
 end
