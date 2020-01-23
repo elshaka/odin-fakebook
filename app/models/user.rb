63e816c7a40b6bc7142a4e11898bcb6582ccb06f
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-23 18:02:01 -0400

    Added user#friend_request_sent? and user#friend_request_received? methods. Added validations to friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
-  has_many :friendships
-  has_many :reverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :reverse_friendships, class_name: 'Friendship', foreign_key: :friend_id, dependent: :destroy
 
   devise :database_authenticatable, :registerable, :validatable, :rememberable
 
@@ -17,17 +17,16 @@ class User < ApplicationRecord
     User.where(id: friends_ids)
   end
 
-  def pending_friends
-    pending_friends_ids = friendships.where(confirmed: false).pluck(:friend_id)
-    User.where(id: pending_friends_ids)
-  end
-
   def friend?(user)
     friends.include?(user)
   end
 
-  def pending_friend?(user)
-    pending_friends.include?(user)
+  def friendship_request_sent?(user)
+    friendships.find_by(friend: user, confirmed: false)
+  end
+
+  def friendship_request_received?(user)
+    reverse_friendships.find_by(user: user, confirmed: false)
   end
 
   private
