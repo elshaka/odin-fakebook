630dee43a815f55f6cfb015548c12ed8fcd0957d
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-03-11 12:24:36 -0400

    Remove arrow notation usage on notifications.js

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -2,7 +2,7 @@ function check() {
   $.get('/notifications');
 }
 
-$(document).on('turbolinks:load', () => {
+$(document).on('turbolinks:load', function() {
   clearInterval(window.interval);
   window.interval = setInterval(check, 5000);
 });
